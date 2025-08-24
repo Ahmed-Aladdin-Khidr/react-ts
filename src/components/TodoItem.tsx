1742ddafbc017937277b1510227289a54a00078a
Ahmed Aladdin <ahmed3laa3132@gmail.com>
2025-08-24 19:01:45 +0300

    Refactor Todos component to utilize TodoItem for rendering, improving code modularity and readability.

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Todo from '../models/todos';
+
+const TodoItem: React.FC<{ item: Todo }> = (props) => {
+    return <li key={props.item.id}>{props.item.text}</li>;
+}
+
+export default TodoItem;
